e7d24dde61da7ba25e46f12780cc440d3c625a75
J3eff <jeffersonvideo125@gmail.com>
2022-08-16 00:24:53 -0300

    25. Algo prático com argumentos

---
+++ 2_FUNDAMENTOS/6_pratica_com_args/index.js
@@ -0,0 +1,14 @@
+// externo
+const minimist = require("minimist")
+
+// interno
+const soma = require('./soma').soma
+
+const args = minimist(process.argv.slice(2))
+
+const a = parseInt(args["a"])
+const b = parseInt(args["b"])
+
+soma(a, b)
+
+//node index.js --a=5 --b=10
