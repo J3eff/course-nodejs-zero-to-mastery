96e26b92870c3e0e21b2344e0b3333132f15eae3
J3eff <jeffersonvideo125@gmail.com>
2022-08-17 00:48:22 -0300

    32. Sync e Async

---
+++ 2_FUNDAMENTOS/13_sync_async/assincrono.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+console.log('Inicio')
+
+fs.writeFile("arquivo.txt", "oi", function(err) {
+    setTimeout(function() {
+        console.log('Arquivo criado')
+    }, 1000)
+})
+
+console.log('Fim')
