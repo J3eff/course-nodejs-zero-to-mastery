24955a04b86e5420cc59757067a994d71387639a
J3eff <jeffersonvideo125@gmail.com>
2022-08-16 23:58:45 -0300

    Parar 28. Lendo input de dados 2

---
+++ 2_FUNDAMENTOS/9_lendo_input/index.js
@@ -0,0 +1,13 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Qual a sua linguagem preferida? ", (language) => {
+    if (language === "html" || language === "HTML") {
+        console.log("Isso nem é linguagem");
+    } else {
+        console.log(`A minha linaguem preferida é: ${language}`)
+        readline.close();
+    }
+})
