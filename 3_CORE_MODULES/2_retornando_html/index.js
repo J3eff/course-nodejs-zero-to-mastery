3782efc9f8cffb61d3006f8fc9dddea1a9a3ae77
J3eff <jeffersonvideo125@gmail.com>
2022-08-17 23:28:45 -0300

    39. Retornando HTML 1

--- 3_CORE_MODULES/2_retornando_html/index.js
+++ 3_CORE_MODULES/2_retornando_html/index.js
@@ -5,7 +5,7 @@ const port = 3000
 const server = http.createServer((req,res) => {
   res.statusCode = 200
   res.setHeader('Contenty-Type', 'text/html')
-  res.end('<h1>Olá, este é meu primeiro server com HTML!</h1>')
+  res.end('<h1>Olá, este é meu primeiro server com HTML!</h1><p>Testando atualização</p>')
 })
 
 server.listen(port, () =>{
