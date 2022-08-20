f92b24cc6e367e8788d3c9dddf20961c619f5678
J3eff <jeffersonvideo125@gmail.com>
2022-08-20 13:05:05 -0300

    47. Renomeando arquivos

---
+++ 3_CORE_MODULES/9_renomear_arquivo/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const arqAntigo = "arquivo.txt"
+const arqNovo = "novoarquivo.txt"
+
+fs.rename(arqAntigo, arqNovo, function(err){
+    if(err){
+        console.log(err)
+        return
+    }
+
+    console.log("Arquivo renomeado!")
+})
