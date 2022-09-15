67eed1349a94235a9b9735247c7853a8f4a9ffbd
J3eff <jeffersonvideo125@gmail.com>
2022-09-15 00:49:23 -0300

    Parar

--- 3_CORE_MODULES/8_removendo_arquivos/index.js
+++ 3_CORE_MODULES/8_removendo_arquivos/index.js
@@ -1,10 +1,10 @@
-const fs = require('fs')
+const fs = require("fs");
 
-fs.unlink('arquivo.txt', function(err){
-    if(err){
-        console.log(err)
-        return
-    }
+fs.unlink("arquivo.txt", function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    console.log('Arquivo removido!')
+  console.log("Arquivo removido!");
 });
