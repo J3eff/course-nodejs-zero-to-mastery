e7f478cfc291167bc74cb369dad0aed6c7a2069e
J3eff <jeffersonvideo125@gmail.com>
2022-09-17 18:00:54 -0300

    71. Criação de conta

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -15,10 +15,21 @@ function operation() {
         choices: ['Criar Conta', 'Consultar Salado', 'Depositar', 'Sacar', 'Sair']
     }])
         .then((anwser) => {
-
             const action = anwser['action']
 
-            console.log(action)
+            switch (action) {
+                case 'Criar Conta':
+                    createAccount()
+                    break
+                default:
+                    break
+            }
 
         }).catch((err) => console.log(err))
+}
+
+// create an account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
 }
