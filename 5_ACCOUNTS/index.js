c30fdd49f73067fa53ef07598be24956e108c232
J3eff <jeffersonvideo125@gmail.com>
2022-09-17 18:16:23 -0300

    72. Finalizando criação de conta

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -32,4 +32,34 @@ function operation() {
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta:'
+        }
+    ]).then(anwser => {
+        const accountName = anwser['accountName']
+
+        console.info(accountName)
+
+        if (!fs.existsSync('accounts'))
+            fs.mkdirSync('accounts')
+
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function (err) {
+            console.log(err)
+        })
+
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+        operation()
+    }).catch(err => console.log(err))
 }
