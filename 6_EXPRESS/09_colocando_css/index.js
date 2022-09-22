8e82b2626265876abb92372f0b04b26767869a46
J3eff <jeffersonvideo125@gmail.com>
2022-09-22 14:09:54 -0300

    90. Colocando CSS com Express

---
+++ 6_EXPRESS/09_colocando_css/index.js
@@ -0,0 +1,26 @@
+const path = require("path")
+const express = require("express")
+const app = express()
+const usersRoute = require('./users')
+
+app.listen(3000, () => { console.log(`App rodando na porta ${3000}`) })
+
+// ler o body
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
+
+// arquivos estáticos
+app.use(express.static('public'))
+
+const basePath = path.join(__dirname, 'templates')
+
+app.use('/users', usersRoute)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
