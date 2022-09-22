a3ccf19a41a2395bb92f9808893ad69e04101174
J3eff <jeffersonvideo125@gmail.com>
2022-09-22 12:19:44 -0300

    88. Lendo dados de um POST

---
+++ 6_EXPRESS/7_post_de_dados/index.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const express = require("express")
+const app = express()
+
+app.listen(3000, () => { console.log(`App rodando na porta ${3000}`) })
+
+// ler o body
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
+
+const basePath = path.join(__dirname, 'templates')
+
+app.get('/users/add', (req, res) => {
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+app.post('/users/save', (req, res) => {
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`O nome do usuário é ${name} e ele tem ${age} anos`)
+
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+
+    // leitura da tabela users, resgatar um usuário do banco
+    console.log(`Estamos buscando pelo usuário: ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
