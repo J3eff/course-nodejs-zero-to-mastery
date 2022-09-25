475cfe3d07923912f825b1bc36fe86733d8e2e10
J3eff <jeffersonvideo125@gmail.com>
2022-09-25 15:20:06 -0300

    100. Condicionais no Handlebars

---
+++ 7_TEMPLATE_ENGINE/4_condicionais/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard")
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        name: "Jefferson",
+        surname: "Brandão",
+        age: 23
+    }
+
+    const palavra = "Teste"
+
+    const auth = true
+
+    res.render('home', { user: user, palavra, auth })
+})
+
+app.listen(3000, () => { console.log(`App funcionando!`) })
