213e8c9ccb3fb97a97ae0f8465d836f193dc4ad2
J3eff <jeffersonvideo125@gmail.com>
2022-09-25 22:33:34 -0300

    103. Utilizando o with

---
+++ 7_TEMPLATE_ENGINE/7_with/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get("/dashboard", (req, res) => {
+    const items = ["Item a", "Item b", "Item c",]
+
+    res.render("dashboard", { items })
+})
+
+app.get("/post", (req, res) => {
+    const post = {
+        title: "Aprender Node.Js",
+        category: "JavaScript",
+        body: "Este artigo vai te ajudar a aprender Node.js...",
+        comments: 4
+    }
+
+    res.render("blogpost", { post })
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        name: "Jefferson",
+        surname: "Brandão",
+        age: 23
+    }
+
+    const palavra = "Teste"
+
+    const auth = true
+
+    const approved = false
+
+    res.render('home', { user: user, palavra, auth, approved })
+})
+
+app.listen(3000, () => { console.log(`App funcionando!`) })
