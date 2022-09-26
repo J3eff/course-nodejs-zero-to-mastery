6bfb6f3d9a8a5422dbb558cbc07ffb90d6a8ef23
J3eff <jeffersonvideo125@gmail.com>
2022-09-25 22:13:56 -0300

    102. Estrutura de repetição

---
+++ 7_TEMPLATE_ENGINE/6_estrutura_repeticao/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get("/dashboard", (req, res) => {
+    const items = ["Item a", "Item b", "Item c",]
+
+    res.render("dashboard", { items })
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        name: "Jefferson",
+        surname: "Brandão",
+        age: 23
+    }
+
+    const palavra = "Teste"
+
+    const auth = true
+
+    const approved = false
+
+    res.render('home', { user: user, palavra, auth, approved })
+})
+
+app.listen(3000, () => { console.log(`App funcionando!`) })
