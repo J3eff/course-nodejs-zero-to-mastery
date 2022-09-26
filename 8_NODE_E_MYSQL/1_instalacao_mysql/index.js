ce6354c936c7c521a0882369fe0ffaf4937c6efa
J3eff <jeffersonvideo125@gmail.com>
2022-09-26 18:59:17 -0300

    115. Conectando o Node ao MySQL

---
+++ 8_NODE_E_MYSQL/1_instalacao_mysql/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql")
+
+const app = express()
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql'
+})
+
+conn.connect(function (err) {
+    if (err)
+        console.log(err)
+
+    console.log('Conectou ao MySQL')
+
+    app.listen(3000)
+})
