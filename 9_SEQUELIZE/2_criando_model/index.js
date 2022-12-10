f436046b25b9d5d78681b3b183876c1b6517532e
J3eff <jeffersonvideo125@gmail.com>
2022-12-10 18:19:20 -0300

    140. Adicionando dado relacionado

--- 9_SEQUELIZE/2_criando_model/index.js
+++ 9_SEQUELIZE/2_criando_model/index.js
@@ -90,6 +90,25 @@ app.get('/', async (req, res) => {
     res.render('home', { users: users })
 })
 
+app.post('/address/create', async (req, res) => {
+    const UserId = req.body.UserId
+    const street = req.body.street
+    const number = req.body.number
+    const city = req.body.city
+
+    const address = {
+        UserId,
+        street,
+        number, 
+        city
+    
+    }
+
+    await Address.create(address);
+
+    res.redirect(`/users/edit/${UserId}`)
+})
+
 conn
     .sync()
     //.sync({force: true})
