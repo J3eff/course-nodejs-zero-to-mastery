d39c7975dcacacf55dde04e130a560ef9fa57f31
J3eff <jeffersonvideo125@gmail.com>
2023-01-06 17:31:14 -0300

    152. Criando as rotas do projeto

--- 10_MVC/controller/TaskController.js
+++ 10_MVC/controller/TaskController.js
@@ -2,7 +2,11 @@ const Task = require('../models/Task')
 
 
 module.exports = class TaskController {
-    static createTask(req, res){
+    static createTask(req, res) {
         res.render('tasks/create')
+    }
+
+    static showTasks(req, res) {
+        res.render('tasks/all')
     }
 }
--- 10_MVC/index.js
+++ 10_MVC/index.js
@@ -6,9 +6,12 @@ const conn = require('./db/conn')
 
 const Task = require('./models/Task')
 
+const tasksRoutes = require('./routes/tasksRoutes')
+
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+// Definindo a biblioteca que faz o parsing dos conteúdos da requisições que o express recebe
 app.use(
     express.urlencoded({
         extended: true
@@ -16,8 +19,10 @@ app.use(
 )
 
 app.use(express.json())
+
 app.use(express.static('public'))
 
+app.use('/tasks', tasksRoutes)
 
 conn
     .sync()
