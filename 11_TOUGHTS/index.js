d93e853c299ee8caf8f46f2065461d4aef36510a
J3eff <jeffersonvideo125@gmail.com>
2023-01-09 00:43:45 -0300

    164. Finalizando estrutura

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -5,8 +5,53 @@ const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
 
 const app = express()
-
 const conn = require('./db/conn')
+
+// template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+// receber reposta do body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+// session middleware
+app.use(
+    session({
+        name: "session",
+        secret: "nosso_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function () { },
+            path: require('path').join(require('os').tmpdir(), 'sessions')
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    })
+)
+
+// flash messages
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+// set session to res
+app.use((req, res, next) => {
+    if (req && req.session && req.session.userid)
+        res.locals.session = req.session
+
+    next()
+})
 
 conn
     .sync()
