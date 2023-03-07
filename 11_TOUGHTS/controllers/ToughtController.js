f3627debf74446e6e398b6cd06a61f3ffa249bb7
J3eff <jeffersonvideo125@gmail.com>
2023-03-07 00:15:26 -0300

    178. Inserindo Pensamentos no banco de dados - 2:33

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -6,11 +6,30 @@ module.exports = class ToughtController {
         res.render('toughts/home')
     }
 
-    static async dashboard(req, res){
+    static async dashboard(req, res) {
         res.render('toughts/dashboard')
     }
 
-    static createTought(req, res){
+    static createTought(req, res) {
         res.render('toughts/create')
+    }
+
+    static async createToughtSave(req, res) {
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userId
+        }
+
+        try {
+            await Toughts.create(tought)
+
+            req.flash('message', 'Pensamento criado com sucesso!')
+
+            req.session.save(() => {
+                res.renderect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log('Aconteceu um erro: ' + error)
+        }
     }
 }
--- 11_TOUGHTS/routers/toughtsRoutes.js
+++ 11_TOUGHTS/routers/toughtsRoutes.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 const ToughtController = require('../controllers/ToughtController')
 
-router.get('/add', ToughtController.createTought)
-router.get('/dashboard', ToughtController.dashboard)
+// helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/add', checkAuth, ToughtController.createTought)
+router.post('/add', checkAuth, ToughtController.createToughtSave)
+router.get('/dashboard', checkAuth, ToughtController.dashboard)
 router.get('/', ToughtController.showToughts)
 
 
