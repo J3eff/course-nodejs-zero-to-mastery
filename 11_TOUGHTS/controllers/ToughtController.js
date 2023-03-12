f83372e88db6f4449acc252a873d59303fa95d68
J3eff <jeffersonvideo125@gmail.com>
2023-03-12 12:45:52 -0300

    181. Removendo Pensamentos

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -48,4 +48,23 @@ module.exports = class ToughtController {
             console.log('Aconteceu um erro: ' + error)
         }
     }
+
+    static async removeTought(req, res) {
+        const id = req.body.id;
+        const userId = req.session.userid;
+
+        try {
+            await Tought.destroy({ where: { id: id, UserId: userId } })
+
+            req.flash('message', 'Pensamento removido com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log('Aconteceu um erro: ' + error)
+        }
+
+        res.redirect
+    }
 }
