45313f26fc53b761a1d5a5138e85e0adcd940c5b
J3eff <jeffersonvideo125@gmail.com>
2023-06-08 15:24:38 -0300

    211. Criando formulário de edição

--- 12_MONGODB/controller/ProductController.js
+++ 12_MONGODB/controller/ProductController.js
@@ -32,10 +32,18 @@ module.exports = class ProductController {
     }
 
     static async removeProduct(req, res) {
-        const id  = req.params.id;
+        const id = req.params.id;
 
         await Product.removeProductById(id)
 
         res.redirect('/products');
+    }
+
+    static async editProduct(req, res) {
+        const id = req.params.id;
+
+        const product = await Product.getProductById(id);
+
+        res.render('products/edit', { product })
     }
 }
