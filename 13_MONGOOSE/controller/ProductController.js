d5bdaf8b85f554e79ca9c85f6cbd3e0bd5ca0739
J3eff <jeffersonvideo125@gmail.com>
2023-06-08 22:34:30 -0300

    219. Inserindo dados

--- 13_MONGOOSE/controller/ProductController.js
+++ 13_MONGOOSE/controller/ProductController.js
@@ -1,62 +1,62 @@
 const Product = require('../models/Product')
 
 module.exports = class ProductController {
-    static async showProducts(req, res) {
-        const products = await Product.getProducts();
+    // static async showProducts(req, res) {
+    //     const products = await Product.getProducts();
 
-        res.render('products/all', { products })
-    }
+    //     res.render('products/all', { products })
+    // }
 
     static createProducts(req, res) {
         res.render('products/create')
     }
 
-    static createProductsPost(req, res) {
-
+    static async createProductsPost(req, res) {
         const name = req.body.name;
         const price = req.body.price;
         const image = req.body.image;
         const description = req.body.description;
 
-        const product = new Product(name, price, description, image);
-        product.save();
+        const product = new Product({name, price, description, image});
+
+        await product.save();
 
         res.redirect('/products')
     }
 
-    static async getProduct(req, res) {
-        const id = req.params.id;
-        const product = await Product.getProductById(id);
+    // static async getProduct(req, res) {
+    //     const id = req.params.id;
+    //     const product = await Product.getProductById(id);
 
-        res.render('products/product', { product })
-    }
+    //     res.render('products/product', { product })
+    // }
 
-    static async removeProduct(req, res) {
-        const id = req.params.id;
+    // static async removeProduct(req, res) {
+    //     const id = req.params.id;
 
-        await Product.removeProductById(id)
+    //     await Product.removeProductById(id)
 
-        res.redirect('/products');
-    }
+    //     res.redirect('/products');
+    // }
 
-    static async editProduct(req, res) {
-        const id = req.params.id;
+    // static async editProduct(req, res) {
+    //     const id = req.params.id;
 
-        const product = await Product.getProductById(id);
+    //     const product = await Product.getProductById(id);
 
-        res.render('products/edit', { product })
-    }
+    //     res.render('products/edit', { product })
+    // }
 
-    static async editProductPost(req, res) {
-        const id = req.body.id;
-        const name = req.body.name;
-        const image = req.body.image;
-        const price = req.body.price;
-        const description = req.body.description;
+    // static async editProductPost(req, res) {
+    //     const id = req.body.id;
+    //     const name = req.body.name;
+    //     const image = req.body.image;
+    //     const price = req.body.price;
+    //     const description = req.body.description;
 
-        const product = new Product(name, price, description, image);
-        await product.updateProduct(id);
+    //     const product = new Product(name, price, description, image);
+    //     await product.updateProduct(id);
 
-        res.redirect('/products');
-    }
+    //     res.redirect('/products');
+    // }
 }
