6e402a10e312615cf5b095542ddd4090d615bec8
J3eff <jeffersonvideo125@gmail.com>
2023-06-19 00:12:56 -0300

    235. Status na resposta da API

--- 14_API/index.js
+++ 14_API/index.js
@@ -9,13 +9,14 @@ app.post('/create/product', (req, res) => {
     const name = req.body.name;
     const price = req.body.price;
 
-    res.json({ message: `O produto ${name} foi criado com sucesso!`})
+    if (!name)
+        res.status(422).json({ message: 'O campo nome é obrigatório!' })
+
+    res.status(201).json({ message: `O produto ${name} foi criado com sucesso!` })
 })
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Primeira rota criada com sucesso!'
-    })
+    res.status(200).json({ message: 'Primeira rota criada com sucesso!' })
 })
 
 app.listen(3000)
