5bf5cdd0f391ae6d1a688fa91b64fcec0a7b6b60
J3eff <jeffersonvideo125@gmail.com>
2023-06-19 00:06:55 -0300

    234. Criando rota com verbo POST

--- 14_API/index.js
+++ 14_API/index.js
@@ -5,6 +5,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // rotas - endpoints
+app.post('/create/product', (req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
+
+    res.json({ message: `O produto ${name} foi criado com sucesso!`})
+})
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Primeira rota criada com sucesso!'
