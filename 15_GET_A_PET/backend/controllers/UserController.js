2d5534eae25f812e258b33dd7663cb58a94cb7de
J3eff <jeffersonvideo125@gmail.com>
2023-08-23 23:51:31 -0300

    Retornando token JWT

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const createUserToken = require('../helpers/create-user-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -52,10 +53,8 @@ module.exports = class UserController {
 
         try {
             const newUser = await user.save();
-            res.status(201).json({
-                message: 'Usuário criado!',
-                newUser
-            })
+            
+            await createUserToken(newUser, req, res);
         } catch (error) {
             res.status(500).json({ message: error })
         }
