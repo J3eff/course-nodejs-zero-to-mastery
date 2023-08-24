c7309c1959bbcfd18ba8061dcde7f7ac59ff812f
J3eff <jeffersonvideo125@gmail.com>
2023-08-24 00:02:07 -0300

    Função de login no sistema

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -53,10 +53,40 @@ module.exports = class UserController {
 
         try {
             const newUser = await user.save();
-            
+
             await createUserToken(newUser, req, res);
         } catch (error) {
             res.status(500).json({ message: error })
         }
+    }
+
+    static async login(req, res) {
+        const { email, password } = req.body;
+
+        if (!email) {
+            res.status(422).json({ message: 'O email é obrigatório' })
+            return;
+        }
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatório' })
+            return;
+        }
+
+        // check if user exists
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            res.status(422).json({ message: 'Não há usuário cadastrado com este e-mail!' })
+            return
+        }
+
+        // check if password match with db password
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if (!checkPassword) {
+            res.status(422).json({ message: 'Senha inválida!' })
+            return
+        }
+
+        await createUserToken(user, req, res);        
     }
 }
