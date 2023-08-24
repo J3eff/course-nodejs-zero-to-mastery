84aac52f4e0b38f082d17e2229f83908a2548476
J3eff <jeffersonvideo125@gmail.com>
2023-08-23 23:43:55 -0300

    Registrando usuário no sistema

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -24,19 +25,39 @@ module.exports = class UserController {
         if (!confirmPassword) {
             res.status(422).json({ message: 'A confirmação de senha é obrigatório' })
             return;
-        }         
-        if(password !== confirmPassword){
+        }
+        if (password !== confirmPassword) {
             res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais!' })
             return;
-        } 
+        }
 
         // check if user exists
-        const userExist = await User.findOne({ email: email});
-        if(userExist){
-            res.status(422).json({ message: 'Por favor, utilize outro e-mail!'})
+        const userExist = await User.findOne({ email: email });
+        if (userExist) {
+            res.status(422).json({ message: 'Por favor, utilize outro e-mail!' })
             return
         }
-      
 
+        // create a password
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        // create a user
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash
+        })
+
+        try {
+            const newUser = await user.save();
+            res.status(201).json({
+                message: 'Usuário criado!',
+                newUser
+            })
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
     }
 }
